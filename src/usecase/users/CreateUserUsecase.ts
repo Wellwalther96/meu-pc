1a3a7ad754659ec5b87d07ef0b234668dc9c235a
Well santos <wellington.santos@duque.g12.br>
2025-07-07 19:26:34 -0300

    Correcao do usuario

--- src/usecase/users/CreateUserUsecase.ts
+++ src/usecase/users/CreateUserUsecase.ts
@@ -16,6 +16,22 @@ export class CreateUserUsecase{
         if(existingPhone){
             throw new Error('Telefone já cadastrado')
         }
+         const hashedPassword = await hash(data.password, 10)
+
+        const user: User = {
+            id: uuidv4(),
+            name: data.name,
+            email: data.email,
+            password: hashedPassword,
+            phone: data.phone,
+            created_at: new Date(),
+            posts: [],
+            comments: []
+        }
+
+        await this.userRepository.create(user)
+
+        return user
     }
 }
 
